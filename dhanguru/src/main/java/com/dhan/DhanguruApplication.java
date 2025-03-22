071a1e7b277d2d41c837619869a303602b6f4616
Krishna <krishna.kapoor337@gmail.com>
2025-03-22 13:31:35 +0530

    initial commit

---
+++ dhanguru/src/main/java/com/dhan/DhanguruApplication.java
@@ -0,0 +1,13 @@
+package com.dhan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhanguruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhanguruApplication.class, args);
+	}
+
+}
